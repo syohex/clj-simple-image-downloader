6603f064a8ad1633e369db71a8bf38e9e0bad2e2
Syohei YOSHIDA <syohex@gmail.com>
2015-03-30 23:43:15 +0900

    Initial implement

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject image-downloader "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simple image downloader"
+  :url "http://github.com/syohex/clj-image-downloader"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-http "1.1.0"]]
+  :main image-downloader.core)
--- src/image_downloader/core.clj
+++ src/image_downloader/core.clj
@@ -1,6 +1,36 @@
-(ns image-downloader.core)
+(ns image-downloader.core
+  (:require [clj-http.client :as client]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(use 'clojure.java.io)
+
+(defn- get-content [url]
+  (let [res (client/get url)]
+    (if (= (:status res) 200)
+      (:body res))))
+
+(defn- get-content-as-byte-array [url]
+  (let [res (client/get url {:as :byte-array})]
+    (if (= (:status res) 200)
+      (:body res))))
+
+(defn- retrieve-images [content]
+  (for [[_ img] (re-seq #"a href=\"([^\"]+)\"" content)
+        :when (re-find #"\.je?pg$" img)]
+    img))
+
+(defn- download-images [url]
+  (let [content (get-content url)]
+    (when content
+      (for [image (retrieve-images content)]
+        (do
+          (println "Download " image)
+          (let [name (.getName (as-file image))
+                image-content (get-content-as-byte-array image)]
+            (when image-content
+              (with-open [w (output-stream name)]
+                (.write w image-content)))))))))
+
+(defn -main
+  [& urls]
+  (for [url urls]
+    (download-images url)))
